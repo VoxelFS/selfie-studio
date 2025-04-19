3ff3713cf61ee35b3601d37ca6861cb6278a2e8e
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-04-19 16:42:45 +1200

    about us component

--- src/app/(frontend)/about/page.tsx
+++ src/app/(frontend)/about/page.tsx
@@ -2,6 +2,7 @@ import StandardLayout from '@/components/layout/StandardLayout';
 import Title from '@/components/Title';
 import { getAboutPage } from '@/actions/pages/getAboutPage'
 import parseAboutPage from '@/types/parsers/parseAboutPage'
+import AboutUs from '@/app/(frontend)/about/_components/AboutUs'
 
 export default async function Page() {
   const aboutPage = parseAboutPage(await getAboutPage());
@@ -12,8 +13,9 @@ export default async function Page() {
   return (
     <>
       <StandardLayout>
-        <div className="mt-12">
+        <div className="mt-12 w-fit h-full">
           <Title title={"about us"} />
+          <AboutUs aboutPage={aboutPage} />
         </div>
       </StandardLayout>
     </>
