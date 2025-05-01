9a7f43488b4e378d96c5c0d1d9abe50381908f4f
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-05-01 13:52:37 +1200

    added more revalidate routes

--- src/actions/revalidateRoutes.ts
+++ src/actions/revalidateRoutes.ts
@@ -4,6 +4,10 @@ import { revalidatePath } from 'next/cache'
 
 export async function revalidateAll() {
   revalidatePath('/')
+  revalidatePath('/about')
+  revalidatePath('/locations')
+  revalidatePath('/gallery')
+  revalidatePath('/contact')
 
   // Add any dynamic revalidation logic here, e.g., pull slugs and loop
   console.log('Revalidated all paths')
