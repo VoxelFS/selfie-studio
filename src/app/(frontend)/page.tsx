4d10ed3a16c160dd933f0f5e945df7b25dc4101b
owenli <102705391+VoxelFS@users.noreply.github.com>
2025-06-10 18:09:10 +1200

    photos now stored locally

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,16 +1,14 @@
 import HomePageLayout from "@/components/layout/HomePageLayout";
-import { getHomePage } from "@/actions/pages/getHomePage";
 import Image from "next/image";
-import parseHomePage from "@/types/parsers/parseHomePage";
 import Carousel from "@/components/Carousel";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Link from "next/link";
 
 export default async function HomePage() {
-    const homePage = parseHomePage(await getHomePage());
-    if (!homePage) {
-        return;
-    }
+
+    const images = [
+        { src: "/assets/homepage/photo1.png", alt: "Home Photo" },
+    ];
 
     return (
         <>
@@ -24,8 +22,8 @@ export default async function HomePage() {
                             Cherish the moment. Relive it later.
                         </h1>
                         <Link
-                            href="/locations"
-                            className="group inline-flex items-center gap-2 py-2 px-4 md:px-6 md:text-base rounded-2xl font-medium bg-[#bbfcff] text-sm md:text-2xl transition-transform transform hover:scale-105"
+                          href="/locations"
+                          className="group inline-flex items-center gap-2 py-2 px-4 md:px-6 md:text-base rounded-2xl font-medium bg-[#bbfcff] text-sm md:text-2xl transition-transform transform hover:scale-105"
                         >
                             Visit Our Locations
                             <ArrowForwardIcon className="w-6 h-6" />
@@ -36,22 +34,22 @@ export default async function HomePage() {
                         showArrow={false}
                         autoSlideInterval={5000}
                     >
-                        {homePage.photos.map((photo, index) => (
-                            <div
-                                key={index}
-                                className="flex-shrink-0 w-screen h-screen relative"
-                            >
-                                <Image
+                        <div
+                            className="flex-shrink-0 w-screen h-screen relative"
+                        >
+                            {images.map((photo, index) => (
+                              <div key={index} className="flex-shrink-0 w-screen h-screen relative">
+                                  <Image
                                     src={photo.src}
                                     alt={photo.alt}
                                     fill
                                     className="object-cover"
                                     priority
-                                />
-
-                                <div className="absolute inset-0 bg-black opacity-75 z-10" />
-                            </div>
-                        ))}
+                                  />
+                                  <div className="absolute inset-0 bg-black opacity-75 z-10" />
+                              </div>
+                            ))}
+                        </div>
                     </Carousel>
                 </div>
             </HomePageLayout>
